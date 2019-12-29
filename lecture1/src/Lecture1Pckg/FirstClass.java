dbe08004939036717d11918ad30ee16a5fa02d1a
ziaullahmughal <59131718+ziaullahmughal@users.noreply.github.com>
2019-12-29 10:03:43 +0500

    291219B4Class

---
+++ lecture1/src/Lecture1Pckg/FirstClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lecture1Pckg;
+
+/**
+ *
+ * @author zia
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("this is my first output private");
+        int a,b,c,d,e,f;
+        a = 1;
+        b = 1;
+        c = 1;
+        d = 1;
+        e = 1;
+        f = 1;
+        System.out.println(++a + b++ + c++ + a++);
+    }
+    
+}
